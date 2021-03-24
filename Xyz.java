69fba93a95510f685d3c367ecc981f5702eafeea
meghanavelagala <velagalameghana99@gmail.com>
2021-03-24 17:12:41 +0530

    Altered abc,java and constructed Xyz.java

---
+++ Xyz.java
@@ -0,0 +1,11 @@
+Public class Xyz
+{
+	private int x;
+	private String str;
+	Public void setstr()
+	{
+	}
+	public void setX()
+	{
+	}
+}
--- abc.java
+++ abc.java
@@ -3,5 +3,7 @@ public class abc
 	public static void main(String args[])
 	{
 		System.out.println("HELLO");
+		for(int i=0;i<=10;i++)
+			System.out.println("HI");
 	}
 }
